6bcab02ea504d7e76a634686b703af929c2ee277
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-05 11:15:32 -0400

    Update not-in-example.py

--- not-in-example.py
+++ not-in-example.py
@@ -1,3 +1,29 @@
 # Abe Lincoln
 # 15 AUG 20XX
 # Not and In Membership Example
+
+# Example 1
+fruits = ["apple", "banana", "orange"]
+
+if "grape" not in fruits:
+  print("Grape is not in the fruit list.")
+else:
+  print("Grape is in the fruit list.")
+
+
+# Example 2
+word = "hello"
+
+if "x" not in word:
+  print("The letter 'x' is not in the word 'hello'.")
+else:
+  print("The letter 'x' is in the word 'hello'.")
+
+
+# Example 3
+sentence = "I love to eat pizza"
+
+if "burger" not in sentence:
+  print("The word 'burger' is not in the sentence.")
+else:
+  print("The word 'burger' is in the sentence.")
