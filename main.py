038d3f51382baa72822361f325b786974c9e92c3
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-05 11:24:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 # Student name
 # Current date
-# Project name
+# Testing for Membership with the 'in' Operator
+
+# Part 1
+# Create a list of your friends
+# Add at least five (5) friends to your list
+# Use an if-else statement and the 'in' operator to check for the name
+# of a specific friend in the list
+# Print a message confirming that specific friend is in the list
+# Otherwise, print a different message saying that the friend is NOT in the list
+
+
+
+
+
+
+# Part 2
+# Assign your last name (as a string) to a variable
+# Use an if-else statement to check if the letter 'm' is present in your last name
+# Tell Python to print a message about the letter 'm' based on the result of the membership check
+
+
+
+
+
+
+# Part 3
+# Create a list containing at least four (4) different flavors of ice cream
+# Use the built-in `input()` function to prompt (ask) the user for their favorite flavor of ice cream
+# Use an if-else statement and the `not in` operators to see if the user's favorite flavor of ice
+# cream is present in the list of ice cream flavors
+# Print a short message that tells the user whether his/her favorite flavor was present in the 
+# list of flavors (or not)
